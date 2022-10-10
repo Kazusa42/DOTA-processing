add85a8136c25c2f3b00323b3323be15a20cc612
Kitahara Kazusa <94526092+Kazusa42@users.noreply.github.com>
2022-10-10 14:48:03 +0900

    Update pre_process.py

--- pre_process.py
+++ pre_process.py
@@ -1,21 +1,21 @@
 from crop import splitbase
 from post_process import txt2xml, delEmptyFile, layoutTestTxt
 
-BASE_DATA = [r'C:/Users/Kazusa/Desktop/DOTA_TRAIN_BASE/',
-             r'C:/Users/Kazusa/Desktop/DOTA_VAL_BASE/']
+BASE_DATA = [r'C:/Users/Kazusa/Desktop/DOTA/TRAIN/',
+             r'C:/Users/Kazusa/Desktop/DOTA/VAL/',
+             r'C:/Users/Kazusa/Desktop/DOTA/TEST/']
 
 MULTICROP_DATA = [r'C:/Users/Kazusa/Desktop/DOTA_TRAIN_MULTICROP/',
                   r'C:/Users/Kazusa/Desktop/DOTA_VAL_MULTICROP/']
 
-SINGLECROP_DATA = [r'C:/Users/Kazusa/Desktop/DOTA_TRAIN_SINGLE/',
-                   r'C:/Users/Kazusa/Desktop/DOTA_VAL_SINGLE/']
+SINGLECROP_DATA = [r'C:/Users/Kazusa/Desktop/DOTA/TRAIN_SINGLE/',
+                   r'C:/Users/Kazusa/Desktop/DOTA/VAL_SINGLE/',
+                   r'C:/Users/Kazusa/Desktop/DOTA/TEST_SINGLE/']
 
 
-MULTICROP_PARA = {'train': [(480, 100), (1280, 256)],
-                  'test': [(480, 100), (1280, 100)]}
+MULTICROP_PARA = [(480, 100), (1280, 256)]
 
-SINGLECROP_PARA = {'train': [(1280, 50)],
-                   'test': [(1280, 50)]}
+SINGLECROP_PARA = [(1280, 50)]
 
 
 def cropWithParams(crop_paras, base_path, out_path):
@@ -33,16 +33,16 @@ if __name__ == '__main__':
     """
     Single crop
     """
-    # cropWithParams(SINGLECROP_PARA['train'], base_path=BASE_DATA[0], out_path=SINGLECROP_DATA[0])
-    # cropWithParams(SINGLECROP_PARA['test'], base_path=BASE_DATA[1], out_path=SINGLECROP_DATA[1])
+    for i in range(len(BASE_DATA)):
+        cropWithParams(SINGLECROP_PARA, base_path=BASE_DATA[i], out_path=SINGLECROP_DATA[i])
 
     """
     Multi crop
     """
-    # cropWithParams(MULTICROP_PARA['train'], base_path=BASE_DATA[0], out_path=MULTICROP_DATA[0])
-    # cropWithParams(MULTICROP_PARA['test'], base_path=BASE_DATA[1], out_path=MULTICROP_DATA[1])
+    """for i in range(len(BASE_DATA)):
+        cropWithParams(MULTICROP_PARA, base_path=BASE_DATA[i], out_path=MULTICROP_DATA[i])"""
 
     # Creat test.txt under ./Project1/DOTA/ImageSets/Main
     # print('Layout test.txt for evaluate.')
-    layoutTestTxt(MULTICROP_DATA[1])
+    # layoutTestTxt(MULTICROP_DATA[1])
     # txt2xml(BASE_DATA[1])
